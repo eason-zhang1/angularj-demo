2d9046fd955452f75a5ab70d1f33d8a2662c2d9a
张海军 <739663407@qq.com>
2017-09-29 17:31:22 +0800

    init angular-demo

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu.component';
+import { WeatherComponent } from './weather/weather.component';
+import { CurrencyComponent } from './currency/currency.component';
+import { MovieComponent } from './movie/movie.component';
+import { CONST_ROUTING } from './app.routing'; // 新添加
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    WeatherComponent,
+    CurrencyComponent,
+    MovieComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    CONST_ROUTING // 新添加
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
